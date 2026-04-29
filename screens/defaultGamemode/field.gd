156de21257ead31e81597baf1ecdf40fd7728788
quentintyr <quentin.tyr@gmail.com>
2026-04-29 19:27:41 +0200

    FEAT | implemented the new field script - player selection - display icons of each player - player switching

---
+++ screens/defaultGamemode/field.gd
@@ -0,0 +1,45 @@
+extends GridContainer
+
+@onready var game = get_node("..")
+@onready var buttons = [
+	$fieldZero,
+	$fieldOne,
+	$fieldTwo,
+	$fieldThree,
+	$fieldFour,
+	$fieldFive,
+	$fieldSix,
+	$fieldSeven,
+	$fieldEight
+]
+
+var x_icon = load("res://assets/player_x.png")
+var o_icon = load("res://assets/player_o.png")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	for i in range(buttons.size()):
+		buttons[i].pressed.connect(_on_field_pressed.bind(i))
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func buttonLogicHelper() -> void:
+	print("")
+
+func _on_field_pressed(index: int) -> void:
+	var row = index / 3
+	var col = index % 3
+	
+	if game.boardArray[row][col] != game.cellState.EMPTY:
+		return
+
+	if game.isPlayerXTurn:
+		buttons[index].icon = x_icon
+		game.boardArray[row][col] = game.cellState.X
+	else:
+		buttons[index].icon = o_icon
+		game.boardArray[row][col] = game.cellState.CIRCLE
+
+	game.player_turn_switch()
